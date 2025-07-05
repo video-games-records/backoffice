b800c3c0f287c853c2ffe8b76090fbd3a240a905
magicbart <magicbart@gmail.com>
2025-07-05 09:27:26 +0200

    Composer update vgr & pn bundles

--- config/bundles.php
+++ config/bundles.php
@@ -2,9 +2,9 @@
 
 return [
     ProjetNormandie\ForumBundle\ProjetNormandieForumBundle::class => ['all' => true],
-    ProjetNormandie\MessageBundle\ProjetNormandieMessageBundle::class => ['all' => true],
-    ProjetNormandie\PartnerBundle\ProjetNormandiePartnerBundle::class => ['all' => true],
-    ProjetNormandie\TwitchBundle\ProjetNormandieTwitchBundle::class => ['all' => true],
+    //ProjetNormandie\MessageBundle\ProjetNormandieMessageBundle::class => ['all' => true],
+    //ProjetNormandie\PartnerBundle\ProjetNormandiePartnerBundle::class => ['all' => true],
+    //ProjetNormandie\TwitchBundle\ProjetNormandieTwitchBundle::class => ['all' => true],
     VideoGamesRecords\CoreBundle\VideoGamesRecordsCoreBundle::class => ['all' => true],
     VideoGamesRecords\DwhBundle\VideoGamesRecordsDwhBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
